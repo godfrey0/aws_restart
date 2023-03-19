33c07027b496df5303f0f9e3c7659894ddb5430a
Godfrey Ong <80189918+godfrey0@users.noreply.github.com>
2023-03-19 19:21:50 +0800

    Add files via upload

---
+++ lab108.py
@@ -0,0 +1,10 @@
+
+def even_odd(number):
+    if(number%2 == 0):
+        print("number is even")
+    else:
+        print("number is odd")
+even_odd(450)
+even_odd(101)
+    
+
---
+++ numericdatatype.py
@@ -0,0 +1,13 @@
+"""
+Your module description
+"""
+a=2
+b=3
+print(a+b)
+
+def area_of_circle(radius):
+    area=3.14*radius*radius
+    print(area)
+area_of_circle(7)
+area_of_circle(14)
+area_of_circle(10)
